7e37238f522f2a4309b1309868b6c16ebd54104a
kaimikan <oihic@protonmail.com>
2022-04-12 22:24:33 +0200

    Handlebars templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,52 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
-// index.html is a default main path name so we don't need to add it
-const publicDirectoryPath = path.join(__dirname, "../public");
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.get("/weather", (req, res) => {
-  res.send({
-    forecast: "Good weather",
-    location: "Deventer",
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather Chat",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    message: "this is a message",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    error: "Help article not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    error: "404: Page Not Found",
   });
 });
 
