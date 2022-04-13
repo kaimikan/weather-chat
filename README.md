097733e8535acbb201e4ef585f39186cc85180d1
kaimikan <oihic@protonmail.com>
2022-04-13 12:11:59 +0200

    Heroku setup

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # weather-chat
 
-An app with the purpose of chatting and sharing weather information using Node JS, Express, and MongoDB. Using references from Andrew Mead's Node.js course.
+An app with the purpose of chatting and sharing weather information using Node JS, Express and others. Using references from Andrew Mead's Node.js course.
 
-to run app:
+Check out the app [here](https://weather-chat-app.herokuapp.com/).
+
+to run app locally:
 
 ```
-nodemon src\app.js -e js,hbs
+npm run start
+```
+
+to develop:
+
+```
+npm run dev
 ```
 
 open localhost:3000 afterwards
--- public/js/app.js
+++ public/js/app.js
@@ -13,19 +13,17 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          console.log(data.error);
-          messageOne.textContent = data.error;
-        } else {
-          console.log(data.location);
-          messageOne.textContent = data.location;
-          console.log(data.forecast);
-          messageTwo.textContent = `${data.forecast.description} - Temp: ${data.forecast.temperature}°C, Feels Like: ${data.forecast.feelslike}°C`;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+        messageOne.textContent = data.error;
+      } else {
+        console.log(data.location);
+        messageOne.textContent = data.location;
+        console.log(data.forecast);
+        messageTwo.textContent = `${data.forecast.description} - Temp: ${data.forecast.temperature}°C, Feels Like: ${data.forecast.feelslike}°C`;
+      }
+    });
+  });
 });
